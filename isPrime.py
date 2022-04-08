3d66bf3dd921d074eac968630c17d3c24cbe014e
KoukeFoxes <koukefoxes@gmail.com>
2022-04-08 19:11:46 +0800

    add question summary

--- isPrime.py
+++ isPrime.py
@@ -1,3 +1,12 @@
+"""
+    前6個質數分別是2, 3, 5, 7, 11, 16, 請問排在所有質數中的第1001個質數是？
+    
+    需求說明：
+    1. 使用Python 3設計程式解題
+    2. 程式請放至Google Colab，檔名設為prime_xxxxxxxx.ipynb (其中xxxxxxxx請用自己的學號)，再將分享連結上傳至本作業。
+    
+"""
+
 import sys
 
 def main() -> None:
