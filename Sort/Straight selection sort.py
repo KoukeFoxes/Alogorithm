875feaa236acb51bb5b97962e1aa4c3a2d006432
KoukeFoxes <koukefoxes@gmail.com>
2022-04-23 12:31:47 +0800

    Update Straight selection sort.py

--- Sort/Straight selection sort.py
+++ Sort/Straight selection sort.py
@@ -1,10 +1,11 @@
-def SSS(list: list) -> list:
+def SSS(list: list):
     for i in range(len(list)-1):
         min_index = i
         for j in range(i+1,len(list)):
             if list[j] < list[min_index]:
                 min_index = j
         list[i],list[min_index] = list[min_index],list[i]
-    return list
-
-print(SSS([7,5,1,4,3,2,6]))
+    
+list = [7,5,1,4,3,2,6]
+SSS(list)
+print(list)
